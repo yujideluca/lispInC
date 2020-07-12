13e73dd6d762ab15878297c73248d292c25fa8d7
yujideluca <35408668+yujideluca@users.noreply.github.com>
2020-07-12 18:46:35 -0300

    Add files via upload

--- lisp.c
+++ lisp.c
@@ -2,7 +2,9 @@
 //commands that start with "#" character are preprocessor commands
 #include <stdio.h>
 #include <stdlib.h>
-
+ /*quotation marks searches the curent directory first, while angular
+   brackets searches the system locations first*/
+#include "mpc.h"
 //if we are compiling on windows, compile these functions
 #ifdef _WIN32
 #include <string.h>
@@ -18,7 +20,8 @@ char* readline(char* prompt) {
     cpy[strlen(cpy)-1] = '\0';
     return cpy;
 }
-
+/*quotation marks searches the curent directory first, while angular
+  brackets searches the system locations first*/
 //fake add_history
 void add_history(char* unused) {}
 
@@ -28,42 +31,68 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
-//parser adjective
-/*mpc_parser_t* Adjective = mpc_or(4, //list of adjectives
-    mpc_sym("wow"), mpc_sym("many"),
-    mpc_sym("so"), mpc_sym("such")
-);*/
+int number_of_nodes(mpc_ast_t* t) {
+  if (t->children_num == 0) { return 1; }
+  if (t->children_num >= 1) {
+    int total = 1;
+    /*This loop applies this fuction recursively, making each call
+      analyse a part of the origial tree as a tree itself. When
+      there is only one children in the function, then its result
+      will be added to te total in the loop that recursivelly called
+      it untill all the sums sulve and the fisrt ayer of function solves
+      and the code resumes.*/
+    for (int i = 0; i < t->children_num; i++) {
+      total = total + number_of_nodes(t->children[i]);
+    }
+    return total;
+  }
+  return 0;
+}
 
-//build a parser 'noun' to recognize things
-/*mpc_parser_t* Noun = mpc_or(5, //list of nouns
-mpc_sym("lisp"), mpc_sym("language"),
-mpc_sym("book"), mpc_sym("build"),
-mpc_sym("c")
-);*/
+/*atoi - converts a char* to a int
+  strcmp - takes as input two char* and if they are equal, returns 0
+  strstr takes as input two char* and returns a pointer to the locaiton
+  of the second in the first, or 0 if the second is not a sub-string
+  of the first*/
 
-/*phrase parser needs to reference the precious
-parsers in order to use */
-/*mpc_parser_t* Phrase = mpc_and(2,
-mpcf_strfold, Adjective, Noun, free);*/
+long eval_op(long x, char* op, long y) {
+  if (strcmp(op, "+") == 0) {return x + y;}
+  if (strcmp(op, "-") == 0) {return x - y;}
+  if (strcmp(op, "*") == 0) {return x * y;}
+  if (strcmp(op, "/") == 0) {return x / y;}
+  return 0;
+}
+long eval(mpc_ast_t* t) {
+  //if it is tagged as number, just return it directly
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+  //the operator is always second child
+  char* op = t->children[1]->contents;
+  
+  long x = eval(t->children[2]); //third child = x
 
-/*specifying if zero or more parsers are required:
-by creating a parser that looks for zero or more occurences
-of another parser, this makes the parser is able to accept an
-infinte amount of arguments*/
-//mpc_parser_t* Doge = mpc_many(mpcf_strfold, Phrase);
+  //passas through the rest of the children
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+  return x;
+}
+
+
 
 //same algorithm in another structure
 //name rules
-mpc_parser_t* Adjective = mpc_new("adjective");
-mpc_parser_t* Noun = mpc_new("noun");
-mpc_parser_t* Sum = mpc_new("sum");
-mpc_parser_t* Digit = mpc_new("digit");
-mpc_parser_t* Number = mpc_new("number");
-mpc_parser_t* Phrase = mpc_new("phrase");
-mpc_parser_t* Doge = mpc_new("doge");
+int main(int argc, char** argv) {
+  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Number    = mpc_new("number");
+  mpc_parser_t* Expr      = mpc_new("expr");
+  mpc_parser_t* Lispy     = mpc_new("lispy");
 
-//definig rules named before
-mpca_lang(MPCA_LANG_DEFAULT,
+  //definig rules named before
+  mpca_lang(MPCA_LANG_DEFAULT,
       /*name of the rule: definition;
         "ab"	    The string ab is required.
         'a'	        The character a is required.
@@ -72,41 +101,52 @@ mpca_lang(MPCA_LANG_DEFAULT,
         'a'*	    Zero or more 'a' are required.
         'a'+	    One or more 'a' are required.
         <abba>	    The rule called abba is required.*/
-      "                                           \
-        adjective : \"wow\" | \"many\"            \
-                  | \"so\" | \"such\";            \
-        noun      : \"lisp\" | \"language\"       \
-                  | \"book\" | \"build\" | \"c\"; \
-        digit     : \"0\" | \"1\" | \"2\" | \"3\" \
-                  | \"4\" | \"5\" | \"6\" | \"7\" \
-                  | \"8\" | \"9\";                \
-        number    : <digit>;                      \
-        sum       : \"+\" <number> <number>;      \                  \
-        phrase    : <adjective> <noun>;           \
-        doge      : <phrase>*;                    \
+      "                                                       \
+        number    : /-?[0-9]+/;                               \
+        operator  : '+' |'-' |'*' |'/';                       \
+        expr      : <number> | '('<operator> <expr>+ ')' ;    \
+        lispy     :  /^/<operator> <expr>+ /$/ ;              \
       ",
-Adjective, Noun, Phrase, Doge);
+      Number, Operator, Expr, Lispy);
 
-mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+  while (1) {
 
-sint main(int argc, char** argv) {
+    char* input = readline(">> ");
+    add_history(input);
 
-    while (1) {
-
-        //Output our prompt (hey) and get input
-        char* input = readline(">>");
-
-        //Adds input to historys
-        add_history(input);
-
-        //Prints input back to user
-        printf("%s\n", input);
-
-        //frees the retrieved data stored in the input variable
-        free(input);
-
+    mpc_result_t r;
+    /*attempt to Padrse the user input calls the mpc_parse function with
+      the Lispy parser and the input string, then copies the parse into r
+      and returns 1 on success or 0 on failiure*/
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      
+      //on success print the AST
+      long result = eval(r.output);
+      printf("%li\n", result);
+      mpc_ast_delete(r.output);
+    } else{
+      //otherwise, print the error
+      mpc_err_print(r.output);
+      mpc_err_delete(r.output);
     }
 
+    //prints the tree structure
+    
+    //// Abstract Syntax Tree (AST) from output.
+    //mpc_ast_t* a = r.output;
+    //printf("Tag: %s\n", a->tag);
+    //printf("Contents: %s\n", a->contents);
+    //printf("Number of children: %i\n", a->children_num);
 
+    //// Get first child.
+    //mpc_ast_t* c0 = a->children[0];
+    //printf("First Child Tag: %s\n", c0->tag);
+    //printf("First Child Contents: %s\n", c0->contents);
+    //printf("First Child Number of Chidren: %i\n", c0->children_num);
+
+    free(input);
+  }
+    //undefine and delete parsers
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
